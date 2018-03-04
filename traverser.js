8a4ef73b72089ab0dca177fd621b84f6848b96b5
Michal Takac <takacmichal92@gmail.com>
2018-03-04 17:45:47 +0100

    add traverser

---
+++ traverser.js
@@ -0,0 +1,64 @@
+/**
+ * So now we have our AST, and we want to be able to visit different nodes with
+ * a visitor. We need to be able to call the methods on the visitor whenever we
+ * encounter a node with a matching type.
+ *
+ *   traverse(ast, {
+ *     Program(node, parent) {
+ *       // ...
+ *     },
+ *
+ *     CallExpression(node, parent) {
+ *       // ...
+ *     },
+ *
+ *     NumberLiteral(node, parent) {
+ *       // ...
+ *     },
+ *   });
+ */
+function traverser(ast, visitor) {
+  // A `traverseArray` function that will allow us to iterate over an array and
+  // call the next function that we will define: `traverseNode`.
+  function traverseArray(array, parent) {
+    array.forEach(child => {
+      traverseNode(child, parent)
+    })
+  }
+
+  // `traverseNode` will accept a `node` and its `parent` node. So that it can
+  // pass both to our visitor methods.
+  function traverseNode(node, parent) {
+    let methods = visitor[node.type]
+
+    // If there is an `enter` method for this node type we'll call it with the
+    // `node` and its `parent`.
+    if (methods && methods.enter) {
+      methods.enter(node, parent)
+    }
+
+    switch (node.type) {
+      case 'Program':
+        traverseArray(node.body, node)
+        break
+      case 'CallExpression':
+        traverseArray(node.params, node)
+        break
+      case 'NumberLiteral':
+      case 'Stringliteral':
+        break
+      default:
+        throw new TypeError(node.type)
+    }
+
+    if (methods && methods.exit) {
+      methods.exit(node, parent)
+    }
+  }
+
+  // We kickstart the traverser by calling `traverseNode` with our ast
+  // with no `parent` because the top level of the AST doesn't have a parent.
+  traverseNode(ast, null)
+}
+
+module.exports = traverser
